aa58f1bbbda812a1f00e80e2fd3ca49d39cd8f3b
Gerben Oolbekkink <g.j.w.oolbekkink@gmail.com>
2015-05-08 15:41:51 +0200

    Show workspace in menu

--- src/main/java/nl/tudelft/context/controller/MenuController.java
+++ src/main/java/nl/tudelft/context/controller/MenuController.java
@@ -3,6 +3,11 @@ package nl.tudelft.context.controller;
 import javafx.scene.control.Menu;
 import javafx.scene.control.MenuBar;
 import javafx.scene.control.MenuItem;
+import javafx.stage.DirectoryChooser;
+import javafx.stage.Window;
+import nl.tudelft.context.workspace.Workspace;
+
+import java.io.File;
 
 /**
  * @author René Vennik <renevennik@gmail.com>
@@ -32,7 +37,14 @@ public class MenuController extends MenuBar {
 
         final Menu fileMenu = new Menu("File");
 
-        MenuItem load = new MenuItem("Load...");
+        MenuItem load = new MenuItem("Load Workspace...");
+        DirectoryChooser directoryChooser = new DirectoryChooser();
+        directoryChooser.setTitle("Select Workspace Folder");
+        load.setOnAction(event -> {
+            Window window = mainController.getRoot().getScene().getWindow();
+            File workspaceDirectory = directoryChooser.showDialog(window);
+            mainController.setWorkspace(new Workspace(workspaceDirectory));
+        });
 
         MenuItem exit = new MenuItem("Exit");
         exit.setOnAction(event -> mainController.exitProgram());
