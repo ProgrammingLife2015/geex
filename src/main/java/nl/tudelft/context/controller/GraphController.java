fd37f98977f42271b4e7aaf54a534f250c351c44
René Vennik <renevennik@gmail.com>
2015-05-09 17:25:04 +0200

    Removed unused imports.

--- src/main/java/nl/tudelft/context/controller/GraphController.java
+++ src/main/java/nl/tudelft/context/controller/GraphController.java
@@ -1,7 +1,6 @@
 package nl.tudelft.context.controller;
 
 import javafx.fxml.FXML;
-import javafx.fxml.Initializable;
 import javafx.scene.Group;
 import javafx.scene.control.Label;
 import javafx.scene.control.ProgressIndicator;
