db6107038a25603ab21270fe75bdf1e7335bb185
Jasper Nieuwdorp <jaspernieuwdorp@hotmail.com>
2015-05-21 12:53:00 +0200

    added Annotation Map

---
+++ src/main/java/nl/tudelft/context/annotation/AnnotationMap.java
@@ -0,0 +1,14 @@
+package nl.tudelft.context.annotation;
+
+import java.util.HashMap;
+
+/**
+ * @author Jasper Nieuwdorp
+ * @version 1.0
+ * @since 21-5-2015
+ */
+public class AnnotationMap extends HashMap<String, Annotation> {
+
+}
+
+
