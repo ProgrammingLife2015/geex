ddac17d533fdc6c0d41be422e59b8a0a1a344031
Gerben Oolbekkink <g.j.w.oolbekkink@gmail.com>
2015-05-22 15:20:05 +0200

    Create Test for OverlayController

---
+++ src/test/java/nl/tudelft/context/controller/OverlayControllerTest.java
@@ -0,0 +1,39 @@
+package nl.tudelft.context.controller;
+
+import de.saxsys.javafx.test.JfxRunner;
+import nl.tudelft.context.controller.overlay.OverlayController;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+/**
+ * @author Gerben Oolbekkink <g.j.w.oolbekkink@gmail.com>
+ * @version 1.0
+ * @since 22-5-2015
+ */
+@RunWith(JfxRunner.class)
+public class OverlayControllerTest {
+
+    protected static OverlayController overlayController;
+
+    /**
+     * Setup Overlaycontrollertest
+     * @throws Exception
+     */
+    @BeforeClass
+    public static void beforeClass() throws Exception {
+
+        overlayController = new OverlayController();
+
+    }
+
+    /**
+     * Test RuntimeException on wrong FXML file.
+     */
+    @Test(expected = RuntimeException.class)
+    public void testWrongFXMLFile() {
+
+        overlayController.loadFXML("");
+
+    }
+}
