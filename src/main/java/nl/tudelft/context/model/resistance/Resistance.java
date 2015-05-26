2b72829691871e00f2a396c6a1694af0e535061c
Jasper Nieuwdorp <jaspernieuwdorp@hotmail.com>
2015-05-26 12:18:03 +0200

    implemented Resistance.java

---
+++ src/main/java/nl/tudelft/context/model/resistance/Resistance.java
@@ -0,0 +1,9 @@
+package nl.tudelft.context.model.resistance;
+
+/**
+ * @author Jasper on 26-5-2015.
+ * @version 1.0
+ * @since 26-5-2015
+ */
+public class Resistance {
+}
---
+++ src/main/java/nl/tudelft/context/model/resistance/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * @author Jasper on 26-5-2015.
+ * @version 1.0
+ * @since 26-5-2015
+ */
+package nl.tudelft.context.model.resistance;
