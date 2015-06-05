41142c13fe8f6c1cbbb74cc130fc3fc0e511268c
Jasper Boot <mrjasperboot@gmail.com>
2015-06-05 23:30:03 +0200

    NewickParser now only creates the tree and DrawableNewick will draw the tree

---
+++ src/main/java/nl/tudelft/context/drawable/DrawableNewick.java
@@ -0,0 +1,80 @@
+package nl.tudelft.context.drawable;
+
+import nl.tudelft.context.model.newick.Newick;
+import nl.tudelft.context.model.newick.node.AbstractNode;
+import nl.tudelft.context.model.newick.node.DummyNode;
+
+/**
+ * @author Jasper Boot <mrjasperboot@gmail.com>
+ * @version 1.0
+ * @since 05-06-2015
+ */
+public class DrawableNewick extends Newick {
+
+    /**
+     * The Newick tree that is drawn.
+     */
+    final Newick newick;
+    /**
+     * The indentation for the first ancestor.
+     */
+    public static final int START_INDENT = 25;
+    /**
+     * The vertical distances between nodes.
+     */
+    public static final int ROW_HEIGHT = 25;
+    /**
+     * The weights are scaled with this factor to make relative weights noticeable.
+     */
+    public static final double WEIGHT_SCALE = 5e4;
+    /**
+     * Child nodes are at least MIN_WEIGHT pixels moved from their parent.
+     */
+    public static final int MIN_WEIGHT = 30;
+
+    /**
+     * Create a wrapper around the Newick to draw the Newick tree.
+     *
+     * @param newick The Newick tree to draw
+     */
+    public DrawableNewick(final Newick newick) {
+        this.newick = newick;
+
+        addEdge(new DummyNode(), newick.getRoot());
+        newick.getRoot().setTranslateX(START_INDENT);
+
+        newick.getRoot().getChildren()
+                .forEach(this::constructTree);
+    }
+
+    /**
+     * Create a visual tree structure for the node and its children.
+     *
+     * @param node The node
+     */
+    public void constructTree(final AbstractNode node) {
+        addEdge(node.getParent(), node);
+        translateNode(node);
+        node.getChildren()
+                .forEach(this::constructTree);
+    }
+
+    /**
+     * Translates the positions of the node and its children.
+     *
+     * @param node The root node to start with
+     */
+    public void translateNode(final AbstractNode node) {
+        node.setTranslateX(node.getParent().translateXProperty().doubleValue()
+                + MIN_WEIGHT + node.getWeight() * WEIGHT_SCALE);
+    }
+
+    /**
+     * Gets the Newick tree.
+     *
+     * @return The Newick tree
+     */
+    public Newick getNewick() {
+        return newick;
+    }
+}
--- src/main/java/nl/tudelft/context/model/newick/NewickParser.java
+++ src/main/java/nl/tudelft/context/model/newick/NewickParser.java
@@ -19,18 +19,6 @@ public final class NewickParser extends Parser<Newick> {
      * The node factory.
      */
     private NodeParser nodeParser = new NodeParser();
-    /**
-     * The vertical distances between nodes.
-     */
-    public static final int ROW_HEIGHT = 25;
-    /**
-     * The weights are scaled with this factor to make relative weights noticeable.
-     */
-    public static final double WEIGHT_SCALE = 5e4;
-    /**
-     * Child nodes are at least MIN_WEIGHT pixels moved from their parent.
-     */
-    public static final int MIN_WEIGHT = 30;
 
     /**
      * Empty constructor for creating an empty TreeParser.
@@ -42,14 +30,13 @@ public final class NewickParser extends Parser<Newick> {
     @Override
     protected Newick parse(final BufferedReader... readerList) {
         BufferedReader reader = readerList[0];
-        TreeParser tp =
-                new TreeParser(reader);
+        TreeParser tp = new TreeParser(reader);
 
         Tree nwkTree = tp.tokenize("");
         AbstractNode root = nodeParser.getNode(nwkTree.getRoot());
 
         Newick newick = new Newick();
-        getOffspring(nwkTree.getRoot(), root, newick, 0);
+        getOffspring(nwkTree.getRoot(), root, newick);
         newick.setRoot(root);
 
         return newick;
@@ -60,32 +47,19 @@ public final class NewickParser extends Parser<Newick> {
      *
      * @param node   the node, read by the newick parser
      * @param parent the parent node to add this node to as a child
-     * @param newick   the tree to add the nodes and edges to
-     * @param row    the current row (depth) of the node
+     * @param newick the tree to add the nodes and edges to
      * @return the new row (depth) of the next node
      */
-    public int getOffspring(final TreeNode node, final AbstractNode parent, final Newick newick, final int row) {
+    public void getOffspring(final TreeNode node, final AbstractNode parent, final Newick newick) {
         newick.addVertex(parent);
 
-        int ret = row;
-
-        int addRow = 1;
         for (int i = 0; i < node.numberLeaves; i += 1) {
             TreeNode child = node.getChild(i);
             if (child != null) {
-                addRow = 0;
-                AbstractNode n = createNode(child, parent, ret);
-                ret = getOffspring(child, n, newick, ret);
-                parent.addChild(n);
-                if (i > 0) {
-                    addDummy(parent, n, newick);
-                } else {
-                    newick.addEdge(parent, n);
-                }
+                AbstractNode n = createNode(child, parent);
+                getOffspring(child, n, newick);
             }
         }
-
-        return ret + addRow;
     }
 
     /**
@@ -93,32 +67,25 @@ public final class NewickParser extends Parser<Newick> {
      *
      * @param child  the node, read by the newick parser
      * @param parent the parent node to add the child to
-     * @param row    the current row (depth) of the node
      * @return the node as a Node
      */
-    public AbstractNode createNode(final TreeNode child, final AbstractNode parent, final int row) {
+    public AbstractNode createNode(final TreeNode child, final AbstractNode parent) {
         AbstractNode n = nodeParser.getNode(child);
-        n.setParent(parent);
-        double x = parent.translateXProperty().doubleValue() + MIN_WEIGHT + WEIGHT_SCALE * n.getWeight();
-        n.setTranslateX(x);
-        n.setTranslateY(row * ROW_HEIGHT);
+        DummyNode dummy = new DummyNode();
+        connectNodes(parent, dummy);
+        connectNodes(dummy, n);
 
         return n;
     }
 
     /**
-     * Adds a dummy node, so that we can use orthogonal lines.
+     * Adds a child to a parent node and sets the parent for the child node.
      *
-     * @param parent the parent of the node
-     * @param n      the node
-     * @param newick   the tree to add the dummy to
+     * @param parent The parent node
+     * @param child  The child node
      */
-    public void addDummy(final AbstractNode parent, final AbstractNode n, final Newick newick) {
-        AbstractNode dummy = new DummyNode();
-        dummy.setTranslateX(parent.translateXProperty().doubleValue());
-        dummy.setTranslateY(n.translateYProperty().doubleValue());
-        newick.addVertex(dummy);
-        newick.addEdge(parent, dummy);
-        newick.addEdge(dummy, n);
+    public void connectNodes(final AbstractNode parent, final AbstractNode child) {
+        parent.addChild(child);
+        child.setParent(parent);
     }
 }
