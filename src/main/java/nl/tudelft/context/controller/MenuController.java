3f2f4239636c6a52002dea29125d0ae3f0b648a8
Gerben Oolbekkink <g.j.w.oolbekkink@gmail.com>
2015-06-05 19:33:48 +0200

    Style

--- src/main/java/nl/tudelft/context/controller/MenuController.java
+++ src/main/java/nl/tudelft/context/controller/MenuController.java
@@ -8,7 +8,6 @@ import javafx.scene.control.MenuItem;
 import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyCodeCombination;
 import javafx.scene.input.KeyCombination;
-import nl.tudelft.context.workspace.Workspace;
 
 /**
  * @author René Vennik <renevennik@gmail.com>
