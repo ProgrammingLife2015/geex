7819a406ccfc0ed8e3ac2732e45c4670cc5ca8ca
Gerben Oolbekkink <g.j.w.oolbekkink@gmail.com>
2015-06-05 12:08:18 +0200

    Create loadworkspace function.

--- src/main/java/nl/tudelft/context/controller/WelcomeController.java
+++ src/main/java/nl/tudelft/context/controller/WelcomeController.java
@@ -7,6 +7,7 @@ import javafx.scene.control.ListView;
 import javafx.scene.layout.GridPane;
 import nl.tudelft.context.workspace.Database;
 import nl.tudelft.context.workspace.Workspace;
+import org.tmatesoft.sqljet.core.SqlJetException;
 
 import java.io.File;
 import java.io.FileNotFoundException;
@@ -50,25 +51,32 @@ public class WelcomeController extends ViewController<GridPane> {
 
     @Override
     public void initialize(final URL location, final ResourceBundle resources) {
-        previous.getItems().setAll(Database.instance().getList("workspace", new String[]{"location", "name"}, 5).stream().map(row -> {
-            Label label = new Label(row[1]);
-            label.setOnMouseClicked(event -> {
-                try {
-                    Workspace workspace = new Workspace(new File(row[0]));
-                    workspace.load();
-
-                    mainController.setWorkspace(workspace);
-                    mainController.setBaseView(new NewickController(mainController,
-                            mainController.getMenuController().getLoadGenomeGraph(), workspace.getNewick()));
-                    mainController.displayMessage(MessageController.SUCCESS_LOAD_WORKSPACE);
-                } catch (FileNotFoundException ex) {
-                    mainController.displayMessage(MessageController.FAIL_LOAD_WORKSPACE);
-                }
-            });
-            return label;
-        }).collect(toList()));
+        try {
+            previous.getItems().setAll(Database.instance().getList("workspace", new String[]{"location", "name"}, 5).stream().map(row -> {
+                Label label = new Label(row[1]);
+                label.setOnMouseClicked(event -> loadWorkspace(row[0]));
+                return label;
+            }).collect(toList()));
+        } catch (SqlJetException e) {
+            mainController.displayMessage(MessageController.FAIL_LOAD_PREVIOUS);
+            // Continue, this doesn't break the software.
+        }
 
         load.setOnMouseClicked(event -> Workspace.chooseWorkspace(mainController));
+    }
+
+    private void loadWorkspace(String directory) {
+        try {
+            Workspace workspace = new Workspace(new File(directory));
+            workspace.load();
+
+            mainController.setWorkspace(workspace);
+            mainController.setBaseView(new NewickController(mainController,
+                    mainController.getMenuController().getLoadGenomeGraph(), workspace.getNewick()));
+            mainController.displayMessage(MessageController.SUCCESS_LOAD_WORKSPACE);
+        } catch (FileNotFoundException ex) {
+            mainController.displayMessage(MessageController.FAIL_LOAD_WORKSPACE);
+        }
     }
 
     @Override
