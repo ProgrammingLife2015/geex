6aeff50ecbf3f8a10d4a7b544ca545643d29fed9
Jasper Nieuwdorp <jaspernieuwdorp@hotmail.com>
2015-06-08 21:50:28 +0200

    rewrote the ResistanceParser to accept raw data

--- src/main/java/nl/tudelft/context/model/resistance/ResistanceParser.java
+++ src/main/java/nl/tudelft/context/model/resistance/ResistanceParser.java
@@ -4,11 +4,13 @@ import nl.tudelft.context.model.Parser;
 
 import java.io.BufferedReader;
 import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  * @author Jasper Nieuwdorp
- * @version 1.0
- * @since 26-5-2015
+ * @version 1.1
+ * @since 08-6-2015
  */
 public class ResistanceParser extends Parser<ResistanceMap> {
 
@@ -28,6 +30,11 @@ public class ResistanceParser extends Parser<ResistanceMap> {
         int index = 0;
         while (sc.hasNextLine()) {
             line = sc.nextLine();
+            while (line.matches("^##.*$")) {
+                line = sc.nextLine();
+            }
+            line = line.replace(":", ",");
+            line = line.replace("\r", ",");
             String[] splitLine = line.split(cvsSplitBy);
             resistanceMap.put(index, getResistance(splitLine));
             index++;
@@ -47,10 +54,61 @@ public class ResistanceParser extends Parser<ResistanceMap> {
         String geneName = splitLine[0];
         String typeOfMutation = splitLine[1];
         String change = splitLine[2];
-        int genomePosition = Integer.parseInt(splitLine[3]);
-        String drugName = splitLine[4];
+        String drugName = "none";
+
+        Pattern p = Pattern.compile("(\\d+)(\t)([A-Z])");
+        int genomePosition = Integer.MAX_VALUE;
+        Matcher matcher = p.matcher(splitLine[4]);
+        if (matcher.find()) {
+            genomePosition = Integer.parseInt(matcher.group(1));
+            drugName = getDrugName(matcher.group(3));
+        }
+
         Resistance resistance = new Resistance(geneName, typeOfMutation, change, genomePosition, drugName);
+        System.out.println("resistance = " + resistance);
         return resistance;
+    }
+
+    /**
+     * From a single letter-code determine which drug it stands for.
+     * @param letter String that represents the single letter code
+     * @return String the name of the drug
+     */
+    public final String getDrugName(final String letter) {
+        String drugName;
+        switch (letter) {
+            case "R":
+                drugName = "rifampicin";
+                break;
+            case "T":
+                drugName = "ethionomide";
+                break;
+            case "M":
+                drugName = "ethionomide";
+                break;
+            case "I":
+                drugName = "isoniazid";
+                break;
+            case "O":
+                drugName = "ofloxacin";
+                break;
+            case "S":
+                drugName = "streptomycin";
+                break;
+            case "K":
+                drugName = "kanamycin";
+                break;
+            case "P":
+                drugName = "pyrazinamide";
+                break;
+            case "E":
+                drugName = "ethambutol";
+                break;
+            default:
+                drugName = "none";
+
+        }
+        return drugName;
     }
 
 }
