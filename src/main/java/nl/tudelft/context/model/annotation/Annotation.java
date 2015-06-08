b14476b7da7783d86f32831661d3895549f71ec1
Jasper Nieuwdorp <jaspernieuwdorp@hotmail.com>
2015-06-08 22:41:49 +0200

    started rebuilding annotation parser

--- src/main/java/nl/tudelft/context/model/annotation/Annotation.java
+++ src/main/java/nl/tudelft/context/model/annotation/Annotation.java
@@ -1,41 +1,84 @@
 package nl.tudelft.context.model.annotation;
 
 /**
+ * Annotations for coding part of genome.
+ * Descriptions of fields retrieved from http://www.sequenceontology.org/gff3.shtml
+ *
  * @author Jasper Nieuwdorp
- * @version 1.0
+ * @version 1.1
  * @since 21-5-2015
  */
 public class Annotation {
 
-    /**
-     * The id of the annotation (from Tabel Browser, wrongfully labeled score).
-     */
-    int id;
 
     /**
-     * Name of the genome.
+     * The ID of the landmark used to establish the coordinate system for the current feature.
+     * IDs may contain any characters, but must escape any characters not in the set [a-zA-Z0-9.:^*$@!+_?-|].
+     * In particular, IDs may not contain unescaped whitespace and must not begin with an unescaped ">".
      */
-    String name;
+    String seqid;
 
     /**
-     * Positve (true) or Negative (false) strand (Sense and Anti-Sense).
+     * The source is a free text qualifier intended to describe the algorithm or operating procedure that generated this feature.
+     * Typically this is the name of a piece of software, such as "Genescan" or a database name, such as "Genbank."
+     * In effect, the source is used to extend the feature ontology by adding a qualifier to the type creating a new composite type
+     * that is a subclass of the type in the type column.
      */
-    boolean strand;
+    String source;
 
     /**
-     * Transcription start position.
+     * The type of the feature (previously called the "method").
+     * This is constrained to be either: (a)a term from the "lite" version of the Sequence Ontology
+     * - SOFA, a term from the full Sequence Ontology -
+     * it must be an is_a child of sequence_feature (SO:0000110) or (c) a SOFA or SO accession number.
+     * The latter alternative is distinguished using the syntax SO:000000.
+     */
+    String type;
+
+    /**
+     * The start coordinates of the feature are given in positive 1-based integer coordinates, relative to the landmark given in column one.
+     * Start is always less than or equal to end.
+     * For features that cross the origin of a circular feature (e.g. most bacterial genomes, plasmids, and some viral genomes),
+     * the requirement for start to be less than or equal to end is satisfied by making end = the position of the end + the length of the landmark feature.
+     * For zero-length features, such as insertion sites, start equals end and the implied site is to the right of the indicated base in the direction of the landmark.
      */
     int start;
 
     /**
-     * Transcription end position.
+     * the end coordinate, see start.
      */
     int end;
 
+
     /**
-     * Name of the protein.
+     * The score of the feature, a floating point number. As in earlier versions of the format, the semantics of the score are ill-defined.
+     * It is strongly recommended that E-values be used for sequence similarity features, and that P-values be used for ab initio gene prediction features.
      */
-    String proteinName;
+    float score;
+
+    /**
+     * The strand of the feature.
+     * + for positive strand (relative to the landmark), - for minus strand, and . for features that are not stranded.
+     * In addition, ? can be used for features whose strandedness is relevant, but unknown.
+     */
+    char strand;
+
+    /**
+     * For features of type "CDS", the phase indicates where the feature begins with reference to the reading frame.
+     * The phase is one of the integers 0, 1, or 2, indicating the number of bases that should be removed from the beginning of this feature to reach the first base of the next codon.
+     * In other words, a phase of "0" indicates that the next codon begins at the first base of the region described by the current line,
+     * a phase of "1" indicates that the next codon begins at the second base of this region, and a phase of "2" indicates that the codon begins at the third base of this region.
+     * This is NOT to be confused with the frame, which is simply start modulo 3.
+     * For forward strand features, phase is counted from the start field. For reverse strand features, phase is counted from the end field.
+     * The phase is REQUIRED for all CDS features.
+     */
+    char phase;
+
+
+    /**
+     * A list of feature attributes in the format tag=value.
+     */
+    String attributes;
 
     /**
      * Create an annotation.
