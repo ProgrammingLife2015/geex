09f7ded522ac89ef98e5694f64036feb437507c9
Gerben Oolbekkink <g.j.w.oolbekkink@gmail.com>
2015-06-14 16:55:38 +0200

    Javadoc in message

--- geex-logger/src/main/java/nl/tudelft/context/logger/message/Message.java
+++ geex-logger/src/main/java/nl/tudelft/context/logger/message/Message.java
@@ -6,29 +6,91 @@ package nl.tudelft.context.logger.message;
  * @since 13-6-2015
  */
 public enum Message {
+    /**
+     * Message used when loading previous workspaces fails.
+     */
     FAIL_LOAD_PREVIOUS("Could not load previous workspaces."),
-    FAIL_LOAD_GRAPH("Could not load genome graph."),
-    SUCCESS_LOAD_GRAPH("Genome graph loaded successfully."),
-    FAIL_LOAD_WORKSPACE("Could not load workspace."),
-    SUCCESS_LOAD_WORKSPACE("Workspace loaded successfully."),
-    FAIL_LOAD_TREE("Could not load phylogenetic tree."),
-    SUCCESS_LOAD_TREE("Phylogenetic tree loaded successfully."),
-    FAIL_LOAD_ANNOTATION("Could not load annotations."),
-    SUCCESS_LOAD_ANNOTATION("Annotations loaded successfully."),
-    FAIL_LOAD_RESISTANCE("Could not load resistance annotations."),
-    SUCCESS_LOAD_RESISTANCE("Resistance Annotations loaded successfully."),
-    FAIL_LOAD_RECENTWORKSPACE("Could not load recent workspace."),
-    MESSAGE_READY("Ready"),
-    DEBUGGER_READY("Debug logger ready.");
 
+    /**
+     * Message used when graph loading fails.
+     */
+    FAIL_LOAD_GRAPH("Could not load genome graph."),
+
+    /**
+     * Message used when graph loading succeeds.
+     */
+    SUCCESS_LOAD_GRAPH("Genome graph loaded successfully."),
+
+    /**
+     * Message used when workspace loading fails.
+     */
+    FAIL_LOAD_WORKSPACE("Could not load workspace."),
+
+    /**
+     * Message used when workspace loading succeeds.
+     */
+    SUCCESS_LOAD_WORKSPACE("Workspace loaded successfully."),
+
+    /**
+     * Message used when tree loading fails.
+     */
+    FAIL_LOAD_TREE("Could not load phylogenetic tree."),
+
+    /**
+     * Message used when tree loading succeeds.
+     */
+    SUCCESS_LOAD_TREE("Phylogenetic tree loaded successfully."),
+
+    /**
+     * Message used when annotation loading fails.
+     */
+    FAIL_LOAD_ANNOTATION("Could not load annotations."),
+
+    /**
+     * Message used when annotation loading succeeds.
+     */
+    SUCCESS_LOAD_ANNOTATION("Annotations loaded successfully."),
+    /**
+     * The text that is shown.
+     */
+
+    /**
+     * Message used when resistance loading fails.
+     */
+    FAIL_LOAD_RESISTANCE("Could not load resistance annotations."),
+
+    /**
+     * Message used when annotation loading succeeds.
+     */
+    SUCCESS_LOAD_RESISTANCE("Resistance Annotations loaded successfully."),
+
+    /**
+     * Message used when annotation loading succeeds.
+     */
+    FAIL_LOAD_RECENTWORKSPACE("Could not load recent workspace."),
+
+    /**
+     * Message used when the application is ready.
+     */
+    MESSAGE_READY("Ready"),
+
+    /**
+     * Message used to show debugging is enabled.
+     */
+    DEBUGGER_READY("Debug logger enabled.");
+
+    /**
+     * Message string in this message.
+     */
     private String message;
 
-    Message(String message) {
+    /**
+     * Create a new Message.
+     *
+     * @param message Message string
+     */
+    Message(final String message) {
         this.message = message;
-    }
-
-    public String getMessage() {
-        return message;
     }
 
     @Override
--- geex-logger/src/main/java/nl/tudelft/context/logger/message/MessageType.java
+++ geex-logger/src/main/java/nl/tudelft/context/logger/message/MessageType.java
@@ -11,29 +11,61 @@ import static java.util.stream.Collectors.toList;
  * @since 13-6-2015
  */
 public enum MessageType implements Comparable<MessageType> {
+    /**
+     * Used when showing a warning message.
+     *
+     * A warning message implies that the software had to stop.
+     */
     WARNING(3, "warning"),
+    /**
+     * Used when showing an info message.
+     *
+     * Info messages are used to tell the user that something happened.
+     */
     INFO(2, "info"),
-    DEBUG(1, "debug"),
-    ;
+    /**
+     * Used when showing a debug message.
+     *
+     * Debug messages are used when something goes wrong and is must be debugged.
+     */
+    DEBUG(1, "debug");
 
+    /**
+     * Loglevel of this MessageType.
+     */
     private int level;
+    /**
+     * Type of this MessageType.
+     */
     private String type;
 
-    MessageType(int level, String type) {
+    /**
+     * Create a new MessageType.
+     *
+     * @param level LogLevel of this MessageType
+     * @param type Type name of this MessageType
+     */
+    MessageType(final int level, final String type) {
         this.level = level;
         this.type = type;
     }
 
-    public String getType() {
-        return type;
-    }
-
+    /**
+     * Get the log-level of this MessageType.
+     *
+     * @return log-level
+     */
     public int getLevel() {
         return level;
     }
 
+    /**
+     * Get a list of available types.
+     *
+     * @return List of types in MessageType
+     */
     public static List<String> types() {
-        return Arrays.stream(values()).map(MessageType::getType).collect(toList());
+        return Arrays.stream(values()).map(MessageType::toString).collect(toList());
     }
 
     @Override
