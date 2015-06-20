254f23a2d6017b43619c0bf4736ce81885224c82
Gerben Oolbekkink <g.j.w.oolbekkink@gmail.com>
2015-06-20 17:37:15 +0200

    Test GraphListItem.

--- geex-core/src/test/java/nl/tudelft/context/controller/graphlist/GraphListItemTest.java
+++ geex-core/src/test/java/nl/tudelft/context/controller/graphlist/GraphListItemTest.java
@@ -1,12 +1,20 @@
 package nl.tudelft.context.controller.graphlist;
 
 import de.saxsys.javafx.test.JfxRunner;
+import javafx.beans.InvalidationListener;
+import javafx.beans.Observable;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.EventHandler;
+import javafx.scene.input.MouseButton;
 import javafx.scene.input.MouseEvent;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
 
 /**
  * @author Gerben Oolbekkink
@@ -17,24 +25,26 @@ import org.junit.runner.RunWith;
 public class GraphListItemTest {
 
     @Test
-    public void testMouseClicked() {
-        ObservableList<GraphListItem> list = FXCollections.emptyObservableList();
+    public void testMouseClicked() throws Exception {
+        ObservableList<GraphListItem> list = FXCollections.observableArrayList();
         GraphListItem gli1 = new GraphListItem(GraphFilterEnum.COLLAPSE, list);
         GraphListItem gli2 = new GraphListItem(GraphFilterEnum.INSERT_DELETE, list);
         GraphListItem gli3 = new GraphListItem(GraphFilterEnum.SINGLE_POINT, list);
 
         list.addAll(gli1, gli2, gli3);
 
-        EventHandler eh = gli1.getOnMouseClicked();
+        InvalidationListener mockListener = mock(InvalidationListener.class);
 
-        //eh.handle();
+        gli1.addListener(mockListener);
+
+        EventHandler<? super MouseEvent> eh = gli1.getOnMouseClicked();
+
+        MouseEvent myMouseEvent = new MouseEvent(gli1, gli1, MouseEvent.MOUSE_CLICKED, 0, 0, 0, 0, MouseButton.PRIMARY, 1, true, true, true, true, true, true, true, true, true, true, null);
+
+        eh.handle(myMouseEvent);
+
+        assertTrue(gli1.getStyleClass().contains("active"));
+        assertTrue(gli1.getGraph().isActive());
+        verify(mockListener).invalidated(gli1);
     }
-
-    @Test
-    public void testDeactivate() {
-        GraphListItem gli = new GraphListItem(GraphFilterEnum.COLLAPSE, null);
-
-        gli.
-    }
-
 }
