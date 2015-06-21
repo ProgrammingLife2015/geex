2bd88ae60df19634d1419062293af6107ab93584
Jasper Boot <mrjasperboot@gmail.com>
2015-06-21 15:00:22 +0200

    added more tests and improved zoom testability

--- geex-effects/src/test/java/nl/tudelft/context/effect/ZoomTest.java
+++ geex-effects/src/test/java/nl/tudelft/context/effect/ZoomTest.java
@@ -10,10 +10,12 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import static org.junit.Assert.*;
 import static org.mockito.Mockito.mock;
 
 /**
@@ -33,9 +35,8 @@ public class ZoomTest {
 
     @BeforeClass
     public static void beforeClass() {
-
-        scroll = mock(ScrollPane.class);
-
+        scroll = new ScrollPane();
+        scroll.setMinWidth(1024);
         sequences = mock(Group.class);
         map = new HashMap<>();
         List<Region> labels = new ArrayList<>();
@@ -45,17 +46,27 @@ public class ZoomTest {
         labelProperty = new SimpleObjectProperty<>(labels);
 
         zoom = new Zoom(scroll, sequences, labelProperty);
-
     }
 
-    /**
-     * At least it should give no errors.
-     */
     @Test
-    public void testConstruct() {
+    public void testCalculateBounds() {
+        assertEquals((1.0 - 2.0) * .3, zoom.getBound(1.0, 2.0, .3), 1e-12);
+    }
 
-        zoom = new Zoom(scroll, sequences, labelProperty);
+    @Test
+    public void testListeners() {
+        assertNull(scroll.getOnMouseEntered());
+        assertNull(scroll.getOnMouseMoved());
+        zoom.setEvents(Collections.emptyList());
+        assertNotNull(scroll.getOnMouseEntered());
+        assertNotNull(scroll.getOnMouseMoved());
+    }
 
+    @Test
+    public void testScale() {
+        assertEquals(0, zoom.getScale(5, 10, 15, 20, 25, 30), 1e-6);
+        assertEquals(.416992, zoom.getScale(25, 50, 75, 100, 125, 150), 1e-6);
+        assertEquals(1, zoom.getScale(50, 100, 150, 200, 250, 300), 1e-6);
     }
 
 }
