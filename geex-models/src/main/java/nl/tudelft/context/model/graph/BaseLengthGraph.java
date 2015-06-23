47b55ac3ecd9654e39f9883552986ea770683f8b
Gerben Oolbekkink <g.j.w.oolbekkink@gmail.com>
2015-06-23 11:24:40 +0200

    Create a baselength graph.

---
+++ geex-models/src/main/java/nl/tudelft/context/model/graph/BaseLengthGraph.java
@@ -0,0 +1,35 @@
+package nl.tudelft.context.model.graph;
+
+import java.util.function.Predicate;
+
+/**
+ * @author Gerben Oolbekkink
+ * @version 1.0
+ * @since 23-06-2015
+ */
+public class BaseLengthGraph extends FilterGraph {
+    /**
+     * Minimum unknown ratio to remove.
+     */
+    public static final double THRESHOLD = 10;
+
+    /**
+     * Create a graph with filtered unknown on an other graph.
+     *
+     * @param graph Graph to remove unkowns form.
+     */
+    public BaseLengthGraph(final StackGraph graph) {
+        super(graph);
+    }
+
+    @Override
+    protected Predicate<DefaultNode> filter() {
+        return defaultNode -> defaultNode.getContent().length() <= THRESHOLD;
+    }
+
+    @Override
+    public String getName() {
+        return "Unknown bases (" + (THRESHOLD * 100) + "%)";
+    }
+
+}
