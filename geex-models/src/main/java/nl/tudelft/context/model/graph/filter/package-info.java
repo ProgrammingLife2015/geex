adc1970e50167e5f02e5c0aee66af9c7572b91d7
Gerben Oolbekkink <g.j.w.oolbekkink@gmail.com>
2015-06-23 20:06:19 +0200

    package-info for filter package.

---
+++ geex-models/src/main/java/nl/tudelft/context/model/graph/filter/package-info.java
@@ -0,0 +1,10 @@
+/**
+ * This package contains all filters for the main graph view.
+ * <p/>
+ * Any filter must implement the StackGraphFilter interface.
+ *
+ * @author Gerben Oolbekkink
+ * @version 1.0
+ * @since 23-6-2015
+ */
+package nl.tudelft.context.model.graph.filter;
