d9763de7734acf5e236651602579c7eeaf413059
René Vennik <renevennik@gmail.com>
2015-06-24 22:07:26 +0200

    Remove whitespace.

--- geex-core/src/main/java/nl/tudelft/context/controller/search/NewickSearchController.java
+++ geex-core/src/main/java/nl/tudelft/context/controller/search/NewickSearchController.java
@@ -78,12 +78,10 @@ public class NewickSearchController {
         searchPrev = new Button("\u25b2");
         searchNext = new Button("\u25bc");
 
-
         searchField.setPromptText("Search strains...");
         searchField.getStyleClass().add("searchbox");
         searchPrev.getStyleClass().add(buttonClass);
         searchNext.getStyleClass().add(buttonClass);
-
 
         searchContainer.getChildren().setAll(searchField, searchNext, searchPrev);
 
