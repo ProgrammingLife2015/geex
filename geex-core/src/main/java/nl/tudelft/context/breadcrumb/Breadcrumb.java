cb626b448522b53c5c6070d6894d5b90d07ac8ce
Jasper Boot <mrjasperboot@gmail.com>
2015-06-24 22:34:48 +0200

    First inserted ViewController before emptying viewList

--- geex-core/src/main/java/nl/tudelft/context/breadcrumb/Breadcrumb.java
+++ geex-core/src/main/java/nl/tudelft/context/breadcrumb/Breadcrumb.java
@@ -55,6 +55,7 @@ public final class Breadcrumb extends HBox {
                 .map(createBreadcrumbItem())
                 .collect(Collectors.toList());
 
+        System.out.println("!items.isEmpty() = " + !items.isEmpty());
         if (!items.isEmpty()) {
             items.get(items.size() - 1).getStyleClass().add("last");
         }
